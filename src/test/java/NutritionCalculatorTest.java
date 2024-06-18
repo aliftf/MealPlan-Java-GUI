e7640e8ebb365efd6daa0a472aab85ab78f871a0
raimess11 <rahma.sakti24@gmail.com>
2024-06-18 14:35:18 +0700

    Revert "Add FoodNutritionSetter Test"
    This reverts commit 10e4f744dc215f2a64c46114e8057dd46a1b4e11.

--- src/test/java/NutritionCalculatorTest.java
+++ src/test/java/NutritionCalculatorTest.java
@@ -1,10 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import User.User;
-import Nutrition.FoodNutrition;
 import Nutrition.UserNutrition;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class NutritionCalculatorTest {
@@ -21,25 +19,5 @@ public class NutritionCalculatorTest {
     assertEquals(232.0, calculator.getProtein());
     assertEquals(66.0, calculator.getFat(), 0.1); // Allow a small delta for floating-point calculations
     assertEquals(43.0, calculator.getCarbs(), 0.1); // Assuming remaining calories are carbs
-  }
-  @Test
-  public void testFoodNutritionSetter1() {
-    FoodNutrition food = new FoodNutrition();
-    food.setNutrition(100, 123, 0110, 999999);
-
-    assertEquals(100, food.getCalories());
-    assertEquals(123, food.getProtein());
-    assertEquals(110, food.getCarbs());
-    assertEquals(999999, food.getFat());
-  }
-  @Test
-  public void testFoodNutritionSetter2() {
-    FoodNutrition food = new FoodNutrition();
-    food.setNutrition(100, -123, 0110, 999999);
-
-    assertNotEquals(-123, food.getProtein());
-    assertNotEquals(100, food.getCalories());
-    assertNotEquals(110, food.getCarbs());
-    assertNotEquals(999999, food.getFat());
   }
 }
